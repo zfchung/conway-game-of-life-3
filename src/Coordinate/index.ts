919e00135157c3eb6e390f77be1a731b8fc6de57
ZF Chung (Zack) <zfchung1@gmail.com>
2021-12-15 16:48:56 +0800

    set the boundary to be dynamically determined by extreme coordinates of living cells

--- src/Coordinate/index.ts
+++ src/Coordinate/index.ts
@@ -4,26 +4,28 @@ interface ICoordinate {
 
 export class Coordinate implements ICoordinate {
     private countOfLivingNeighbour: number = 0;
-    private static rowSize: number = 6;
-    private static columnSize: number = 6;
+    private static rowSize: number = 0;
+    private static columnSize: number = 0;
     private readonly row: number;
     private readonly column: number;
 
     constructor(private coordinateId: string) {
         this.row = Coordinate.getRowFromId(coordinateId);
         this.column = Coordinate.getColumnFromId(coordinateId);
+        Coordinate.updateRowSize(this.row);
+        Coordinate.updateColumnSize(this.column);
     }
 
     public getId() {
         return this.coordinateId;
     }
 
-    private static getRowFromId(coordinateId: string){
+    private static getRowFromId(coordinateId: string) {
         const rowAndColumn = coordinateId.split("_");
         return Number(rowAndColumn[0]);
     }
 
-    private static getColumnFromId(coordinateId: string){
+    private static getColumnFromId(coordinateId: string) {
         const rowAndColumn = coordinateId.split("_");
         return Number(rowAndColumn[1]);
     }
@@ -38,7 +40,7 @@ export class Coordinate implements ICoordinate {
                     neighbourRow >= 0 &&
                     neighbourColumn >= 0 &&
                     (neighbourRow != this.row ||
-                    neighbourColumn != this.column)
+                        neighbourColumn != this.column)
                 ) {
                     neighbourList.push(neighbourRow + "_" + neighbourColumn);
                 }
@@ -56,12 +58,12 @@ export class Coordinate implements ICoordinate {
         return this.countOfLivingNeighbour;
     }
 
-    public static getBoardArray(){
+    public static getBoardArray() {
         const resultList: number[][] = [];
 
-        for(let i = 0; i < Coordinate.rowSize; i++){
+        for (let i = 0; i < Coordinate.columnSize; i++) {
             const rowArr = [];
-            for(let i = 0; i < Coordinate.columnSize; i++){
+            for (let i = 0; i < Coordinate.rowSize; i++) {
                 rowArr.push(0);
             }
             resultList.push(rowArr);
@@ -70,4 +72,15 @@ export class Coordinate implements ICoordinate {
         return resultList;
     }
 
+    private static updateRowSize(row: number) {
+        if (Coordinate.rowSize < row) {
+            Coordinate.rowSize = row;
+        }
+    }
+
+    private static updateColumnSize(column: number) {
+        if (Coordinate.columnSize < column) {
+            Coordinate.columnSize = column;
+        }
+    }
 }
