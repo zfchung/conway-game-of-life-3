348187d6388eedefb87dea7fb6bd10a24d590b3b
ZF Chung (Zack) <zfchung1@gmail.com>
2021-12-20 17:32:29 +0800

    extracted getResultArray() function

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,19 +1,23 @@
 // Integration test
 
+function getResultArray(output: string) {
+    const outputList = output.split("\n");
+    const resultString = outputList[1]
+    const resultStringRemovedBracket = resultString.slice(2,-2)
+    const resultStringRemovedSingleQuote = resultStringRemovedBracket.replaceAll("'","")
+    const resultStringRemovedSpace = resultStringRemovedSingleQuote.replaceAll(" ","")
+    return resultStringRemovedSpace.split(",");
+}
+
 describe("testing the result of Game of Life", () => {
     it("should return the shape of toad", () => {
         // Given
         // When
         const execSync = require('child_process').execSync;
         const output = execSync('yarn dev --world="1_2,2_2,3_2"', { encoding: 'utf-8' });
-        const outputList = output.split("\n");
-        const resultString = outputList[1]
-        const result_2 = resultString.slice(2,-2)
-        const result_3 = result_2.replaceAll("'","")
-        const result_4 = result_3.replaceAll(" ","")
-        const result_5 = result_4.split(",")
+        const resultArray = getResultArray(output);
         // Then
         const expectedResult = ["2_1","2_2","2_3"];
-        expect(result_5).toEqual(expect.arrayContaining(expectedResult));
+        expect(resultArray).toEqual(expect.arrayContaining(expectedResult));
     })
 })
