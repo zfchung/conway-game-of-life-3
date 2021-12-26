15ac3c802468e3ae3b6c183a324239c6e8a76cd1
ZF Chung (Zack) <zfchung1@gmail.com>
2021-12-26 13:48:19 +0800

    added each cases for test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,43 +1,45 @@
 // Integration test
 
-function callApplication(world: string[]){
+function callApplication(world: string[]) {
     const worldParsed = world.join(",");
     const execSync = require('child_process').execSync;
-    const output = execSync(`yarn dev --world="${worldParsed}"`, { encoding: 'utf-8' });
+    const output = execSync(`yarn dev --world="${worldParsed}"`, {encoding: 'utf-8'});
     return getResultArray(output);
 }
 
 function getResultArray(output: string) {
     const outputList = output.split("\n");
     const resultString = outputList[1]
-    const resultStringRemovedBracket = resultString.slice(2,-2)
-    const resultStringRemovedSingleQuote = resultStringRemovedBracket.replaceAll("'","")
-    const resultStringRemovedSpace = resultStringRemovedSingleQuote.replaceAll(" ","")
+    const resultStringRemovedBracket = resultString.slice(2, -2)
+    const resultStringRemovedSingleQuote = resultStringRemovedBracket.replaceAll("'", "")
+    const resultStringRemovedSpace = resultStringRemovedSingleQuote.replaceAll(" ", "")
     return resultStringRemovedSpace.split(",");
 }
 
 describe("testing the result of Game of Life", () => {
     it("should return the shape of blinker", () => {
         // Given
-        const liveCellInput = ["1_2","2_2","3_2"];
+        const liveCellInput = ["1_2", "2_2", "3_2"];
         // When
         const resultArray = callApplication(liveCellInput);
         // Then
-        const expectedResult = ["2_1","2_2","2_3"];
+        const expectedResult = ["2_1", "2_2", "2_3"];
         expect(resultArray).toEqual(expect.arrayContaining(expectedResult));
     })
 })
 
-// describe.each([
-//     ["blinker", "1_2,2_2,3_2", ["2_1","2_2","2_3"]]
-// ])('.add(%i, %i)', (shapeName, expectedResult) => {
-//     test(`returns ${expected}`, () => {
-//         // Given
-//         // When
-//         const execSync = require('child_process').execSync;
-//         const output = execSync(`yarn dev --world=${shapeName}`, { encoding: 'utf-8' });
-//         const resultArray = getResultArray(output);
-//         // Then
-//         expect(resultArray).toEqual(expect.arrayContaining(expectedResult));
-//     });
-// });
+describe.each([
+    ["blinker", ["1_2", "2_2", "3_2"], ["2_1", "2_2", "2_3"]],
+    ["block", ["1_1", "1_2", "2_1", "2_2"], ["1_1", "1_2", "2_1", "2_2"]],
+    ["beehive", ["1_2", "1_3", "2_1", "2_4", "3_2", "3_3"], ["1_2", "1_3", "2_1", "2_4", "3_2", "3_3"]],
+    ["beacon", ["1_1", "1_2", "2_1", "2_2", "3_3", "3_4", "4_3", "4_4"], ["1_1", "1_2", "2_1", "3_4", "4_3", "4_4"]],
+    ["toad", ["2_2", "2_3", "2_4", "3_1", "3_2", "3_3"], ["3_1", "2_4", "1_3", "3_4", "4_2", "2_1"]]
+])('testing multiple shapes', (shapeName, liveCells, expectedResult) => {
+    it(`should return the correct tick of ${shapeName}`, () => {
+        // Given
+        // When
+        const resultArray = callApplication(liveCells);
+        // Then
+        expect(resultArray).toEqual(expect.arrayContaining(expectedResult));
+    });
+});
