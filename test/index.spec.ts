0db19db7755c0ef53bb4249f3e60912b24b98f51
ZF Chung (Zack) <zfchung1@gmail.com>
2021-12-26 13:32:57 +0800

    isolated callApplication() from test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,12 @@
 // Integration test
 
+function callApplication(world: string[]){
+    const worldParsed = world.join(",");
+    const execSync = require('child_process').execSync;
+    const output = execSync(`yarn dev --world="${worldParsed}"`, { encoding: 'utf-8' });
+    return getResultArray(output);
+}
+
 function getResultArray(output: string) {
     const outputList = output.split("\n");
     const resultString = outputList[1]
@@ -10,14 +17,27 @@ function getResultArray(output: string) {
 }
 
 describe("testing the result of Game of Life", () => {
-    it("should return the shape of toad", () => {
+    it("should return the shape of blinker", () => {
         // Given
+        const liveCellInput = ["1_2","2_2","3_2"];
         // When
-        const execSync = require('child_process').execSync;
-        const output = execSync('yarn dev --world="1_2,2_2,3_2"', { encoding: 'utf-8' });
-        const resultArray = getResultArray(output);
+        const resultArray = callApplication(liveCellInput);
         // Then
         const expectedResult = ["2_1","2_2","2_3"];
         expect(resultArray).toEqual(expect.arrayContaining(expectedResult));
     })
 })
+
+// describe.each([
+//     ["blinker", "1_2,2_2,3_2", ["2_1","2_2","2_3"]]
+// ])('.add(%i, %i)', (shapeName, expectedResult) => {
+//     test(`returns ${expected}`, () => {
+//         // Given
+//         // When
+//         const execSync = require('child_process').execSync;
+//         const output = execSync(`yarn dev --world=${shapeName}`, { encoding: 'utf-8' });
+//         const resultArray = getResultArray(output);
+//         // Then
+//         expect(resultArray).toEqual(expect.arrayContaining(expectedResult));
+//     });
+// });
